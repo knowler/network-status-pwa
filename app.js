9490d48f79242fb6284d33f5c713f4c9854bd7c5
Nathan Knowler <nathan@knowler.me>
2019-04-07 00:18:57 -0600

    Comments [skip ci]

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ setStatus(navigator.onLine)
 window.addEventListener('offline', () => setStatus(false))
 window.addEventListener('online', () => setStatus(true))
 
+// Add the status element to the app
 app.appendChild(status)
 
+// Register service worker if supported
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+// Create the cache when the service worker installs
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('network-status-v1')
@@ -13,6 +14,7 @@ self.addEventListener('install', event => {
   )
 })
 
+// Request middleware: checks the cache before fetching resources
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request.url) || fetch(event.request.url)
